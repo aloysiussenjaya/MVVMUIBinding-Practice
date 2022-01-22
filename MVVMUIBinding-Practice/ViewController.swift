ac6c398e8e2b1d53090827949f4c6bbd9506dcaf
Aloysius Senjaya <aloysiussenjaya@gmail.com>
2022-01-22 16:19:16 +0700

    Initial Commit

---
+++ MVVMUIBinding-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMUIBinding-Practice
+//
+//  Created by Aloysius Shendy on 22/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
